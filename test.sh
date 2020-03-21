2c59433d2693b4cb52e91f220d3719e3eab3c3e1
Jordan Sokolic <oofnik@gmail.com>
2020-03-21 21:16:40 +0200

    test ipv6 (WIP)

--- test.sh
+++ test.sh
@@ -11,12 +11,15 @@ set -x
 ##                                              ##
 
 run_openwrt() {
+	docker network create iptmon-net --subnet 172.22.0.0/24 --ipv6 --subnet fc22::/64
 	OPENWRT=$(docker run --rm -it -d -v `pwd`:/root/iptmon:ro \
+		--ip 172.22.0.22 \
+		--ip6 fc22::22 \
+		-p 8080:80 \
+		--name test_iptmon \
+		--network iptmon-net \
 		--cap-add net_admin \
-		--sysctl net.ipv6.conf.all.disable_ipv6=0 \
 		--sysctl net.ipv6.conf.all.forwarding=1 \
-		--ip 172.17.0.42/16 \
-    -p 8080:80 \
 		openwrtorg/rootfs:x86-64-19.07.1)
 }
 
@@ -25,12 +28,13 @@ _set_network() {
 	set network.lan=interface
 	set network.lan.ifname=eth0
 	set network.lan.proto=static
-	set network.lan.ipaddr=172.17.0.42/16
+	set network.lan.ipaddr=172.22.0.22/24
+	set network.lan.ip6addr=fc22::22/64
 	set network.default=route
 	set network.default.interface=lan
 	set network.default.target=0.0.0.0
 	set network.default.netmask=0.0.0.0
-	set network.default.gateway=172.17.0.1
+	set network.default.gateway=172.22.0.1
 	commit
 	EOF
 	/etc/init.d/network restart
@@ -39,36 +43,47 @@ _set_network() {
 _install_iptmon() {
 	opkg update
 	opkg install /root/iptmon/build/packages/x86_64/iptmon/iptmon*.ipk
+	/etc/init.d/dnsmasq restart
 }
 
 launch_busybox() {
-	docker run --rm -it busybox udhcpc
+	docker run --rm -it \
+	--network iptmon-net \
+	busybox sh -c '
+		udhcpc -v -x hostname:abcdef
+		udhcpc6
+		ping -c4 fc22::22'
 }
 
 check_iptables() {
-  docker exec -it $OPENWRT sh -c '
-    iptables -t mangle -nvL iptmon_rx
-    iptables -t mangle -nvL iptmon_tx'
+	docker exec -it $OPENWRT sh -c '
+		iptables -t mangle -nvL iptmon_rx
+		iptables -t mangle -nvL iptmon_tx
+		ip6tables -t mangle -nvL iptmon_rx
+		ip6tables -t mangle -nvL iptmon_tx'
 }
 
 main() {
-  docker exec -it $OPENWRT sh -c '
-    . /root/iptmon/test.sh
-    _set_network
-    sleep 3
-    _install_iptmon'
+	docker exec -it $OPENWRT sh -c '
+		. /root/iptmon/test.sh
+		_set_network
+		sleep 3
+		_install_iptmon'
 }
 
 cleanup() {
-  docker kill $OPENWRT
+	docker kill test_iptmon
+	docker network rm iptmon-net
 }
 
 case $1 in
 run)
-  run_openwrt
-  main
-  launch_busybox
-  check_iptables
-  cleanup
+	run_openwrt
+	main
+	launch_busybox
+	check_iptables
+;;
+cleanup)
+	cleanup
 ;;
 esac
