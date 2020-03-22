8bec49f59a24e434c18e7967fb3a8f286abed701
Jordan Sokolic <oofnik@gmail.com>
2020-03-22 09:12:49 +0200

    fix tests

--- test.sh
+++ test.sh
@@ -10,17 +10,25 @@ set -x
 # 4. check for presence of iptmon rules          #
 ##                                              ##
 
+TAG=x86-64-19.07.1
+IP4_ADDR=172.22.0.22
+IP4_GW=172.22.0.1
+IP4_CIDR=172.22.0.0/24
+IP6_ADDR=fc22::22
+IP6_CIDR=fc22::/64
+
 run_openwrt() {
-	docker network create iptmon-net --subnet 172.22.0.0/24 --ipv6 --subnet fc22::/64
+	docker network create iptmon-net --subnet $IP4_CIDR --ipv6 --subnet $IP6_CIDR
 	OPENWRT=$(docker run --rm -it -d -v `pwd`:/root/iptmon:ro \
-		--ip 172.22.0.22 \
-		--ip6 fc22::22 \
+		--ip $IP4_ADDR \
+		--ip6 $IP6_ADDR \
 		-p 8080:80 \
 		--name test_iptmon \
 		--network iptmon-net \
 		--cap-add net_admin \
 		--sysctl net.ipv6.conf.all.forwarding=1 \
-		openwrtorg/rootfs:x86-64-19.07.1)
+		--sysctl net.ipv6.conf.all.disable_ipv6=0 \
+		openwrtorg/rootfs:$TAG)
 }
 
 _set_network() {
@@ -28,13 +36,13 @@ _set_network() {
 	set network.lan=interface
 	set network.lan.ifname=eth0
 	set network.lan.proto=static
-	set network.lan.ipaddr=172.22.0.22/24
-	set network.lan.ip6addr=fc22::22/64
+	set network.lan.ipaddr=${IP4_ADDR}/24
+	set network.lan.ip6addr=${IP6_ADDR}/64
 	set network.default=route
 	set network.default.interface=lan
 	set network.default.target=0.0.0.0
 	set network.default.netmask=0.0.0.0
-	set network.default.gateway=172.22.0.1
+	set network.default.gateway=$IP4_GW
 	commit
 	EOF
 	/etc/init.d/network restart
@@ -43,24 +51,27 @@ _set_network() {
 _install_iptmon() {
 	opkg update
 	opkg install /root/iptmon/build/packages/x86_64/iptmon/iptmon*.ipk
-	/etc/init.d/dnsmasq restart
 }
 
 launch_busybox() {
 	docker run --rm -it \
 	--network iptmon-net \
-	busybox sh -c '
-		udhcpc -v -x hostname:abcdef
+	busybox sh -c "
+		udhcpc -x hostname:abcdef
 		udhcpc6
-		ping -c4 fc22::22'
+	"
 }
 
 check_iptables() {
 	docker exec -it $OPENWRT sh -c '
+		kill -HUP $(pgrep dnsmasq | head -n1)
+		sleep 1
 		iptables -t mangle -nvL iptmon_rx
 		iptables -t mangle -nvL iptmon_tx
 		ip6tables -t mangle -nvL iptmon_rx
-		ip6tables -t mangle -nvL iptmon_tx'
+		ip6tables -t mangle -nvL iptmon_tx
+		logread -l1000 -e iptmon
+	'
 }
 
 main() {
