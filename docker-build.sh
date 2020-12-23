1d3d7d2a6cd75e1983b631753e8370308bd665ab
Jordan Sokolic <oofnik@gmail.com>
2020-12-23 17:53:17 +0200

    tabs
    Signed-off-by: Jordan Sokolic <oofnik@gmail.com>

--- docker-build.sh
+++ docker-build.sh
@@ -7,27 +7,27 @@
 ##                                                                ##
 
 run_sdk() {
-  mkdir -p build
-  chmod a+rwx build
-  docker run --rm -it \
-    -v $(pwd)/build:/home/build/openwrt/bin \
-    -v $(pwd):/home/build/iptmon \
-    openwrtorg/sdk /home/build/iptmon/$0 build_ipk
+	mkdir -p build
+	chmod a+rwx build
+	docker run --rm -it \
+		-v $(pwd)/build:/home/build/openwrt/bin \
+		-v $(pwd):/home/build/iptmon \
+		openwrtorg/sdk /home/build/iptmon/$0 build_ipk
 }
 
 build_ipk() {
-  echo src-link iptmon /home/build/iptmon > feeds.conf
-  make defconfig
-  ./scripts/feeds update iptmon
-  ./scripts/feeds install -a -p iptmon
-  make package/iptmon/compile V=s
+	echo src-link iptmon /home/build/iptmon > feeds.conf
+	./scripts/feeds update
+	./scripts/feeds install iptmon
+	make defconfig
+	make package/iptmon/compile V=s
 }
 
 case $1 in
 build_ipk)
-  build_ipk
+	build_ipk
 ;;
 *)
-  run_sdk
+	run_sdk
 ;;
 esac
